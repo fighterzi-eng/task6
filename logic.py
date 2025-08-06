48c19edd7f6850b5a6ed584b42625df4d9cbac03
john bassem <johnbassem2@gmail.com>
2025-08-07 01:22:59 +0300

    added the logic behind the answer verification

---
+++ logic.py
@@ -0,0 +1,13 @@
+import text_colourer
+def game(word,guess):
+    ur_answer=""
+    for i in range(0,len(guess)):
+        if word[i]==guess[i]:
+            ur_answer+=text_colourer.colouring("g",guess[i])
+        elif guess[i] in word:
+            ur_answer+=text_colourer.colouring("y",guess[i])
+        else:
+            ur_answer+=text_colourer.colouring("r",guess[i])
+
+    return ur_answer
+print(game("hello","homml"))

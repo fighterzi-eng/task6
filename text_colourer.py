c9268e6ae6dc47941adb42aa8efa1a868f846da1
john bassem <johnbassem2@gmail.com>
2025-08-07 00:22:48 +0300

    added a function to color letters

---
+++ text_colourer.py
@@ -0,0 +1,21 @@
+#g is green,r is red,y is yellow
+
+
+def colouring(colour,letter):
+    match colour:
+        case "r":
+            x="\033[31m"+letter+"\033[0m"
+            return x
+
+
+
+        case "y":
+            x="\033[33m"+letter+"\033[0m"
+            return x
+
+
+        case "g":
+            x="\033[32m"+letter+"\033[0m"
+            return x
+print(colouring("g","h"))
+print("h")

9b50ad34f4cc8e4fa301cfdbe31f605a2ca19b27
john bassem <johnbassem2@gmail.com>
2025-08-07 00:04:40 +0300

    created the random word selector

---
+++ word_selector.py
@@ -0,0 +1,23 @@
+
+
+import random
+
+
+f =open("words.txt","r")
+words=[]
+
+
+for line in f:
+
+
+    words.append(line)
+
+def word_selector():
+    x=random.choice(words)
+    return x
+print(word_selector())
+    
+
+
+
+
